ca65c8715f81a955bd789add198bb1889cddd771
Eduardo Aguilar <Agu1laredu@hotmail.com>
2022-06-10 17:01:07 -0300

    agregando arrays

---
+++ app.js
@@ -0,0 +1,21 @@
+function capturar() {
+  function Persona(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+
+  let nombreCapturar = document.getElementById("nombre").value;
+  let edadCapturar = document.getElementById("edad").value;
+
+  nuevoSujeto = new Persona(nombreCapturar, edadCapturar);
+  console.log(nuevoSujeto);
+  agregar();
+}
+
+let baseDatos = [];
+
+function agregar() {
+  baseDatos.push(nuevoSujeto);
+  console.log(baseDatos);
+  document.getElementById("tabla").innerHTML += ' <tbody> <td> ' +nuevoSujeto.nombre+'</td><td></td> ' +nuevoSujeto.edad+' </tbody>';
+}
